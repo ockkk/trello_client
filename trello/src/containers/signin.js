653b7e8333377bc26110175a764a7e0d3518feed
황종옥 <jongock@hwangjong-og-ui-MacBookPro.local>
2019-11-06 02:02:52 +0900

    signin API connect & redirect set

--- trello/src/containers/signin.js
+++ trello/src/containers/signin.js
@@ -1,16 +1,63 @@
-import React from 'react';
+import React, { Component } from 'react'
+import { Redirect } from 'react-router-dom'
 import { Button, Input} from 'reactstrap';
 
-const signin = () => {
-  return (
-    <div>
-      <h2>Trello</h2>
-      <Input type="email" name="paemailssword" id="email" placeholder="email"/>
-      <Input type="password" name="password" id="password" placeholder="password"/>
-      <Button color="info" size="lg" block>Signin</Button>{' '}
-      <Button color="info" size="lg" block>Signup</Button>{' '}
-    </div>
-  );
-};
+export default class signin extends Component {
+  constructor(props){
+    super()
+    this.state = {
+      email: "",
+      password: "",
+      success: null
+    }
+  }
 
-export default signin;
+  validToken = () => {
+    if(!sessionStorage.getItem("token")){
+      this.setState({success: null})
+    }
+  }
+
+  handleEmail = e => {
+    this.setState({
+      email: e.target.value
+    })
+  }
+
+  handlePw = e => {
+    this.setState({
+      password: e.target.value
+    })
+  }
+
+  handelLogin = async e => {
+    console.log(this.state)
+    let loginInfo = {
+      method: "POST",
+      body: JSON.stringify(this.state),
+      headers: {"Content-type": "application/json"}
+      }
+
+    let result = await fetch("http://127.0.0.1:8080/users/signin", loginInfo)
+      .then(date => {return date.json()})
+    
+    this.setState({success: result.success})
+    sessionStorage.setItem("token",result.token)
+    alert(result.message)
+  }
+
+  render() {
+    return (
+      <div>
+      {
+          this.state.success && <Redirect to="/boards"/>
+        }
+      <h2>Trello</h2>
+      <Input type="email" name="paemailssword" id="email" placeholder="email" onChange={this.handleEmail}/>
+      <Input type="password" name="password" id="password" placeholder="password" onChange={this.handlePw}/>
+      <Button color="info" size="lg" block onClick={this.handelLogin}>Signin</Button>
+      <Button color="info" size="lg" block>Signup</Button>
+    </div>
+    )
+  }
+}
