0e0c617e0fd60c6bfb9baecb062fca4017db80b0
황종옥 <jongock@hwangjong-og-ui-MacBookPro.local>
2019-11-13 01:55:18 +0900

    container/ card CRUD function complete

--- trello/src/containers/todoList.js
+++ trello/src/containers/todoList.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react'
+import Signout from "../components/signout"
 import { UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem,Button,  ListGroup, Input, InputGroupAddon, InputGroup } from 'reactstrap';
 import AddContainer from "../components/addCtBtn"
-
 export default class todoList extends Component {
   constructor(props){
     super()
     this.state = {
+      boards: [],
       containers: [],
       cdKey:"",
-      cd_modify:"",
       cd_add:"",
+      cd_modify:"",
       ct_add:"",
       ct_modify:"",
       addCtBtn: false
@@ -17,10 +18,27 @@ export default class todoList extends Component {
   }
 
   componentDidMount(){
+    this.callContainer();
     this.callBoard();
   }
-
   callBoard= async () => {
+    let message = {
+      method: "GET",
+      headers: {
+        "Content-type": "application/json", 
+        token: sessionStorage.getItem("token")}
+      }
+      
+      let boards = await fetch("http://127.0.0.1:8080/boards", message)
+      .then(date => date.json())
+      .catch(err => console.log(err))
+      
+      this.setState({
+      boards: boards
+    })
+  }
+
+  callContainer= async () => {
     let message = {
       method: "GET",
       headers: {
@@ -55,14 +73,19 @@ export default class todoList extends Component {
     })
   }
 
-  handleAddCt= e => {
+  handleChangeBtn= e => {
     this.state.addCtBtn ? this.setState({addCtBtn: false}) : this.setState({addCtBtn:true})
   }
 
   handleAddCtName= e => {
     this.setState({
-      ct_name: "",
       ct_add: e.target.value
+    })
+  }
+
+  handleUpdateCtName= e => {
+    this.setState({
+      ct_modify:e.target.value
     })
   }
 
@@ -70,6 +93,48 @@ export default class todoList extends Component {
     if (e.key === 'Enter') {
       this.updateCard();
     }
+  }
+
+  updateContainer = async e => {
+    if(this.state.ct_modify === ""){
+      alert("수정 사항을 입력해 주세요!!👻")
+      return 
+    }
+
+    let message = {
+      method: "PUT",
+      body: JSON.stringify({"ct_name":this.state.ct_modify}),
+      headers: {
+        "Content-type": "application/json", 
+        token: sessionStorage.getItem("token")}
+      }
+    
+    await fetch(`http://127.0.0.1:8080/containers/${e.target.id}`, message)
+      .then(date => date.json())
+      .then(res => alert(res.message))
+      .catch(err => console.log(err))
+ 
+    this.callContainer();
+    this.setState({
+      ct_modify: ""
+    })
+  }
+
+  deleteContainer= async e => {
+    let ct_key = e.target.id
+    let message = {
+      method: "DELETE",
+      headers: {
+        "Content-type": "application/json", 
+        token: sessionStorage.getItem("token")}
+      }
+    
+    await fetch(`http://127.0.0.1:8080/containers/${ct_key}`, message)
+      .then(date => date.json())
+      .then(res => alert(res.message))
+      .catch(err => console.log(err))
+
+    this.callContainer();
   }
 
   updateCard = async () => {
@@ -90,10 +155,12 @@ export default class todoList extends Component {
       .then(date => date.json())
       .then(res => alert(res.message))
       .catch(err => console.log(err))
+
+    this.callContainer();
     this.setState({
-      cd_modify:""
+      cdKey: "",
+      cd_modify: ""
     })
-    this.callBoard();
   }
 
   addCard= async e => {
@@ -119,7 +186,7 @@ export default class todoList extends Component {
     this.setState({
       cd_add:""
     })
-    this.callBoard();
+    this.callContainer();
   }
 
   deleteCard= async e => {
@@ -136,24 +203,35 @@ export default class todoList extends Component {
       .then(res => alert(res.message))
       .catch(err => console.log(err))
 
-    this.callBoard();
+    this.callContainer();
   }
 
   render() {
     console.log(this.state)
     return (
       <div>
+        <Signout/>
         <UncontrolledDropdown>
           <DropdownToggle caret>
             Boards
-            </DropdownToggle>
+          </DropdownToggle>
           <DropdownMenu>
-          <DropdownItem>Another Action</DropdownItem>
+            <DropdownItem>Another Action</DropdownItem>
           </DropdownMenu>
         </UncontrolledDropdown>
         {this.state.containers.map((con, index) => 
-        <Button color='light' className='border-light' key={con.ct_key}>
-            {con.ct_name}
+        <Button color='light' className='border-light' key={con.ct_key} cardVal={con.cards}>
+          <UncontrolledDropdown >
+            <DropdownToggle caret color="white">
+              {con.ct_name}
+            </DropdownToggle>
+            <DropdownMenu>
+              <Input onChange={this.handleUpdateCtName}/>
+              <DropdownItem id={con.ct_key} onClick={this.updateContainer}>수정</DropdownItem>
+              <DropdownItem divider/>
+              <DropdownItem id={con.ct_key} onClick={this.deleteContainer}>삭제</DropdownItem>
+            </DropdownMenu>
+          </UncontrolledDropdown>
           <ListGroup key={index}>
           {con.cards.map((card, index)=> 
             <InputGroup key={index}>
@@ -175,7 +253,7 @@ export default class todoList extends Component {
                 <InputGroup>
                 <Input type="textarea" placeholder="something to do?" onChange={this.handleAddCdName}/>
                 <InputGroupAddon addonType="append">
-                  <Button color="success" onClick={this.addCard} key>add</Button>
+                  <Button color="success" onClick={this.addCard} id={con.ct_key}>add</Button>
                 </InputGroupAddon>
               </InputGroup>
               }
@@ -184,10 +262,10 @@ export default class todoList extends Component {
         {this.state.addCtBtn ? (<AddContainer b_key = {this.props.match.params.id} 
                                               ct_add = {this.state.ct_add} 
                                               ct_modify = {this.state.ct_modify} 
-                                              click = {this.handleAddCt}
+                                              click = {this.handleChangeBtn}
                                               change= {this.handleAddCtName}
                                               callBoard= {this.callBoard}/>) 
-                                : (<Button onClick={this.handleAddCt}>+ Add another list</Button>)}
+                                : (<Button onClick={this.handleChangeBtn}>+ Add another list</Button>)}
       </div>
     )
   }
